a18d50199a101cfe858e0a400551e532f627ec96
Eesha <eyinamdar@gmail.com>
2021-09-05 16:59:30 +0530

    Added navbar to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/header/Header'
+import Home from './pages/home/Home';
+import Blogs from './pages/blogs/Blog';
+import Contact from './pages/contact/Contact';
+import Events from './pages/events/Events';
+import Teams from './pages/teams/Team';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Upskill</h1>H
-    </div>
-  )
+    <>
+    <Router>
+      <Header />
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+          <Route path="/blogs" component={Blogs} exact></Route>
+          <Route path='/events' component={Events} exact></Route>
+          <Route path='/teams' component={Teams} exact></Route>
+          <Route path='/contact' component={Contact} exact></Route>
+        </Switch>
+    </Router>
+    </>
+  );
 }
-
-export default App
+export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
-import './Header.css'
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+import classes from  './Header.module.css';
+
+const Header = (props) => {
     return (
         <div>
-            <h1>Header</h1>
+        <header className={ classes.header }>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" style={{'padding-left':'10px'}} href="#">
+                    <img src="/images/LOGO.1.png" width="110" height="94" alt="Logo"/>
+                </a>
+            </nav>
+            <nav className={ classes.navbar}>
+                <ul className={ classes.navlinks}>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/teams'>Our Team</Link>
+                    </li>
+                    <li>
+                        <Link to='/events'>Events</Link>
+                    </li>
+                    <li>
+                        <Link to='/blogs'>Blogs</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact Us</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main className={classes.main}>
+         {props.children}
+        </main>
         </div>
-    )
+    );
 }
 
 export default Header
