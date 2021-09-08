9f8d06801851858dd5920dcff15245afecf32774
Aman <amanchopra.atg@gmail.com>
2021-09-09 00:35:44 +0530

    updated teams page

--- src/pages/teams/Team.jsx
+++ src/pages/teams/Team.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Team.css'
+import TeamData from './TeamData'
+import Card from './Card'
 
 const Team = () => {
     return (
         <>
-        
+        <div className="my-3">
+            <h1 className="text-center team-heading my-2">Our Team</h1>
+        </div>
+        <div className="container-fluid mb-5">
+            <div className="row">
+                <div className="col-10 mx-auto">
+                    <div className="row gy-4">
+                        {
+                            TeamData.map((elem) => {
+                                return (
+                                    <Card
+                                    key={elem.id}
+                                    name={elem.name}
+                                    designation={elem.designation}
+                                    imgsrc={elem.imgsrc}
+                                    l_link={elem.l_link} />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/pages/teams/TeamData.jsx
+++ src/pages/teams/TeamData.jsx
@@ -1,24 +1,121 @@
+import aman from "../../images/aman.jpg"
+import aarya from "../../images/aarya.jpeg"
+import aarya1 from "../../images/aarya1.jpeg"
+import abhi from "../../images/abhi.jpeg"
+import gayatri from "../../images/gayatri.jpeg"
+import harnish from "../../images/harnish.png"
+import harshitha from "../../images/harshitha.jpeg"
+import kishore from "../../images/kishore.jpg"
+import meghna from "../../images/meghna.jpg"
+import paras from "../../images/paras.jpeg"
+import sammarth from "../../images/sammarth.jpeg"
+import saad from "../../images/saad.jpeg"
+import saadhna from "../../images/saadhna.jpg"
+
 const TeamData = [
     {
         id: 1,
-        name: "Aarya Patil",
+        name: "Sammarth Patil",
         designation: "Co-Founder",
-        imgsrc:"",
+        imgsrc: sammarth,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171",
     },
 
     {
         id: 2,
         name: "Aarya Patil",
         designation: "Co-Founder",
-        imgsrc:"",
+        imgsrc: aarya,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
     },
     
     {
         id: 3,
-        name: "Aarya Patil",
-        designation: "Co-Founder",
-        imgsrc:"",
-    }
+        name: "Aarya Jayendra Khot",
+        designation: "Administration Head",
+        imgsrc: aarya1,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 4,
+        name: "Meghna M C",
+        designation: "PR Head",
+        imgsrc: meghna,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 5,
+        name: "Harshitha Reddy",
+        designation: "Collaboration Head",
+        imgsrc: harshitha,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+    
+    {
+        id: 6,
+        name: "Aman Chopra",
+        designation: "Program Lead",
+        imgsrc: aman,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 7,
+        name: "Gayatri R Bhosale",
+        designation: "Social Media Head",
+        imgsrc: gayatri,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 8,
+        name: "Saadhana Srinath",
+        designation: "Graphics Head - Linkedin",
+        imgsrc: saadhna,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+    
+    {
+        id: 9,
+        name: "Harnish Savsani",
+        designation: "Graphics Head - Instagram",
+        imgsrc: harnish,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 10,
+        name: "Abhishek Vijay Potekar",
+        designation: "Graphics and Certification Lead",
+        imgsrc: abhi,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 11,
+        name: "Saad R. Sheikh",
+        designation: "Video Editing Lead",
+        imgsrc: saad,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+    
+    {
+        id: 12,
+        name: "Paras Gupta",
+        designation: "Social Media Manager",
+        imgsrc: paras,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
+
+    {
+        id: 13,
+        name: "Kishore Theeraj V J",
+        designation: "Program head",
+        imgsrc: kishore,
+        l_link: "https://www.linkedin.com/in/aman-chopra-375a81171/",
+    },
 ]
 
 export default TeamData
